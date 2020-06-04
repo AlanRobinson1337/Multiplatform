305ebb84cb61a511c625209f4678ffdad1033253
Alan Robinson <30298916@cityofglacol.ac.uk>
2020-06-04 16:33:26 +0100

    Add project files.

---
+++ Events.Data/Migrations/DbMigrationsConfig.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Events.Data.Migrations;
+using Events.Data;
+
+namespace Events.Data.Migrations
+{
+    public sealed class DbMigrationsConfig : DbMigrationsConfiguration<Events.Data.ApplicationDbContext>
+    {
+        public DbMigrationsConfig()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        //seed initial data only if the databse is empty
+        protected override void Seed(ApplicationDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var adminEmail = "admin@admin.com";
+                var adminUserName = adminEmail;
+                var adminFullName = "System Administrator";
+                var adminPassword = adminEmail;
+                string adminRole = "Administrator";
+                CreateAdminUser(context, adminEmail, adminUserName, adminFullName, adminPassword, adminRole);
+                CreateSeveralEvents(context);
+            }
+        }
+
+        private void CreateSeveralEvents(ApplicationDbContext context)
+        {
+            context.Events.Add(new Event()
+            {
+                Title = "Pary @ SoftUni",
+                StartDateTime = DateTime.Now.Date.AddDays(5).AddHours(21).AddMinutes(30),
+                Author = context.Users.First()
+            });
+            context.Events.Add(new Event()
+            {
+                Title = "Past Event <Anonymous",
+                StartDateTime = DateTime.Now.Date.AddDays(-2).AddHours(10).AddMinutes(30),
+                Duration = TimeSpan.FromHours(1.5),
+                Comments = new HashSet<Comment>()
+                {
+                    new Comment() { Text = "<Anonymous> comment" },
+                    new Comment() { Text = "User Comment", Author = context.Users.First() }
+                }
+            });
+        }
+
+        private void CreateAdminUser(ApplicationDbContext context, string adminEmail, string adminUserName, string adminFullName, string adminPassword, string adminRole)
+        {
+            var adminUser = new ApplicationUser
+            {
+                UserName = adminUserName,
+                FullName = adminFullName,
+                Email = adminEmail
+            };
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            userManager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 1,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireUppercase = false,
+                RequireLowercase = false
+            };
+            var userCreateResult = userManager.Create(adminUser, adminPassword);
+            if (!userCreateResult.Succeeded)
+            {
+                throw new Exception(string.Join("; ", userCreateResult.Errors));
+            }
+
+            //create the admin role
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var roleCreateResult = roleManager.Create(new IdentityRole(adminRole));
+            if (!roleCreateResult.Succeeded)
+            {
+                throw new Exception(string.Join("; ", userCreateResult.Errors));
+            }
+
+            //add the admin user to the administrator role
+            var addAdminRoleResult = userManager.AddToRole(adminUser.Id, adminRole);
+            if (!addAdminRoleResult.Succeeded)
+            {
+                throw new Exception(string.Join("; ", userCreateResult.Errors));
+            }
+        }
+    }
+}
